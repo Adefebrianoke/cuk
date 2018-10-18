80c4ef15ad1a9f932d118224910bfdaaee5a8cb2
Adefebrianoke <43769308+Adefebrianoke@users.noreply.github.com>
2018-10-18 18:41:39 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-iif(x=="1"){xxx="2018/10/is-it-time.html"}
-if(x=="2"){xxx="2018/10/how-to.html"}
-if(x=="3"){xxx="2018/10/forex-social.html"}
-if(x=="4"){xxx="2018/10/forex-swing.html"}
-if(x=="5"){xxx="2018/10/trading-price.html"}a_to_ve[i].href="https://safeku.site"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/10/is-it-time.html"}
+if(x=="2"){xxx="/2018/10/how-to.html"}
+if(x=="3"){xxx="/2018/10/forex-social.html"}
+if(x=="4"){xxx="/2018/10/forex-swing.html"}
+if(x=="5"){xxx="/2018/10/trading-price.html"}a_to_ve[i].href="https://safeku.site"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
